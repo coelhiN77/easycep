3b8f871e8a71f3f75a1ffd8b59801b61090c8b23
coelhiN77 <rafaelcoelho2305@hotmail.com>
2023-09-02 02:33:17 -0300

    feat: implementacao para registro de usuario

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('auth/register');
+    }
+
+    public function registerSave(Request $request)
+    {
+        Validator::make($request->all(), [
+            'name' => ['required', Rule::unique('account', 'name')],
+            'email' => ['required', 'email', Rule::unique('account', 'email')],
+            'password' => 'required|confirmed'
+        ])->validate();
+
+        DB::table('account')->insert([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('login');
+    }
+}
