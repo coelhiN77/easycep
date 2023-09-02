1e10596f2ce18752026bdb12cff001f866aaf554
coelhiN77 <rafaelcoelho2305@hotmail.com>
2023-09-02 02:19:16 -0300

    feat: estrutura atualizada da tabela account

--- database/migrations/2023_09_01_020412_create_account_table.php
+++ database/migrations/2023_09_01_020412_create_account_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('account', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
